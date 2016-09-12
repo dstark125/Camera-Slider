0dd9f28d88434f1894741f99daa9b7ce55874c0e
dstark125 <dstark125@gmail.com>
2016-09-11 19:50:03 -0700

    First commit of camera slider code

---
+++ Camera_Slider/Camera_Slider.ino
@@ -0,0 +1,28 @@
+#include <AccelStepper.h>
+#define FULL4WIRE 4
+
+// Motor pin definitions
+#define motorPin1  3     // IN1 on the ULN2003 driver 1
+#define motorPin2  4     // IN2 on the ULN2003 driver 1
+#define motorPin3  5     // IN3 on the ULN2003 driver 1
+#define motorPin4  6     // IN4 on the ULN2003 driver 1
+
+// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
+AccelStepper stepper1(FULL4WIRE, motorPin1, motorPin3, motorPin2, motorPin4);
+
+void setup() {
+  stepper1.setMaxSpeed(500.0);
+  stepper1.setAcceleration(200.0);
+  stepper1.setSpeed(100);
+  stepper1.moveTo(5000);
+
+}//--(end setup )---
+
+void loop() {
+
+  //Change direction when the stepper reaches the target position
+  if (stepper1.distanceToGo() == 0) {
+    stepper1.moveTo(-stepper1.currentPosition());
+  }
+  stepper1.run();
+}
